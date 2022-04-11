703a49108164105b7298047f9ed34df9c949c51d
stutisk <48529714+stutisk@users.noreply.github.com>
2022-04-11 17:47:26 +0530

    feat:added pages

---
+++ src/Pages/Checkoutpage/Checkoutpage.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Checkoutpage = () => {
+  return <>Checkout Page</>;
+};
+
+export {Checkoutpage};
---
+++ src/Pages/Productpage/Productpage.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Productpage = () => {
+  return <>Product Page</>;
+};
+
+export {Productpage};
